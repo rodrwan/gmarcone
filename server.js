1c4f8dcfeacaa818ac871dcee68d662d2fa85667
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-09-27 14:00:05 -0300

    server

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const redis = require("redis");
-const morgan = require("morgan");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const redis = require('redis');
+const morgan = require('morgan');
 
-const Darksky = require("./darksky/darksky");
-const routes = require("./routes/routes");
+const Darksky = require('./darksky/darksky');
+const GeoCode = require('./geocode/geocode');
+const routes = require('./routes/routes');
 
-const { PORT, REDIS_PORT, DARKSKY_TOKEN, CACHE_INTERVAL } = process.env;
+const {
+  PORT,
+  REDIS_PORT,
+  REDIS_HOST,
+  DARKSKY_TOKEN,
+  GEOCODE_API_TOKEN,
+  CACHE_INTERVAL
+} = process.env;
 
 const app = express();
 
-app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
 app.use(bodyParser.json());
-app.use(morgan(":method :url :response-time"));
+app.use(cors());
+app.use(morgan(':method :url :response-time'));
 
-const redisClient = redis.createClient(REDIS_PORT);
+const redisClient = redis.createClient(REDIS_PORT, REDIS_HOST);
 const darkskyClient = new Darksky(DARKSKY_TOKEN, redisClient, CACHE_INTERVAL);
-app.use("/api", routes(darkskyClient));
+const geocodeClient = new GeoCode(GEOCODE_API_TOKEN, redisClient, CACHE_INTERVAL);
+app.use('/api', routes(darkskyClient, geocodeClient));
 
 let port = PORT || 8080;
 // Launch app to listen to specified port
-app.listen(port, function() {
-  console.log("Running gMarcone on port " + port);
+app.listen(port, () => {
+  console.log('Running gMarcone on port ' + port);
 });
