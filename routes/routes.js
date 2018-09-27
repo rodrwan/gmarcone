9962aff5ed3a2f1eb478419b8973873370f80a2f
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-09-27 14:00:18 -0300

    server routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,27 @@
-const router = require("express").Router();
+const router = require('express').Router();
 // Set default API response
 
-const appRouter = darkskyClient => {
-  router.get("/forecast", async (req, res) => {
+const appRouter = (darkskyClient, geocodeClient) => {
+  router.get('/forecast', async (req, res) => {
     const { latitude, longitude, lang } = req.query;
 
-    const result = await darkskyClient.forecast(latitude, longitude, lang);
+    const response = {};
+    const geoResult = await geocodeClient.getData(latitude, longitude, lang);
+    if (!geoResult.hasOwnProperty('name')) {
+      res.status(400).json({ error: 'bad request' });
+      return;
+    }
 
-    res.json(result);
+    Object.assign(response, { ...geoResult });
+    try {
+      const result = await darkskyClient.forecast(geoResult.latitude, geoResult.longitude, lang);
+      Object.assign(response, { ...result });
+    } catch (err) {
+      res.status(500).json({ error: 'internal server error' });
+      return;
+    }
+
+    res.json(response);
   });
 
   return router;
